c9cc0d88f6675f0720c6a23dd25de0e13e05856b
Dwigth32 <dwigth32@outlook.es>
2020-11-18 01:34:54 -0600

    Busqueda e infinite scroller de institutos

--- src/app/modulos/dashboard/pages/instituciones/instituciones.component.ts
+++ src/app/modulos/dashboard/pages/instituciones/instituciones.component.ts
@@ -9,6 +9,17 @@ declare var $: any;
 })
 export class InstitucionesComponent implements OnInit {
 
+  // Infinite scroller
+  throttle = 200;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+  direction = '';
+
+  // Esto solo si no se modifica el tamaño a tomar
+  PREVIUS_SKIP = 0;
+  DEFAULT_TAKE_AMOUNT = 10;
+  TOTAL_AMOUNT = 0;
+
   INSTITUCIONES: any[];
   InstitucionForm = new FormGroup({
     nombre: new FormControl(),
@@ -16,18 +27,31 @@ export class InstitucionesComponent implements OnInit {
     fecha_exp: new FormControl(),
   });
 
+  search(event: KeyboardEvent, text: string) {
+    console.log(event.key);
+
+    if (event.key === 'Enter') {
+      const subs = this.instS.Paginate({ searchParams: { nombre: text, siglas: text } }).subscribe(r => {
+        this.INSTITUCIONES = r[0];
+        subs.unsubscribe();
+      });
+    }
+  }
+
+  onScrollDown() {
+    console.log('scrolling down');
+
+    const subs = this.instS.Paginate({ skip: this.PREVIUS_SKIP += this.DEFAULT_TAKE_AMOUNT }).subscribe(r => {
+      this.INSTITUCIONES = this.INSTITUCIONES.concat(r[0]);
+      subs.unsubscribe();
+    });
+    this.direction = 'down';
+  }
+
   InitData() {
-    const subs = this.instS.GetAll().subscribe(r => {
-      this.INSTITUCIONES = r.data;
-      setTimeout(() => {
-        $('#dataTables-instituciones').DataTable({
-          responsive: true,
-          pageLength: 20,
-          lengthChange: false,
-          searching: true,
-          ordering: true
-        });
-      }, 100);
+    const subs = this.instS.Paginate().subscribe(r => {
+      this.INSTITUCIONES = r[0];
+      this.TOTAL_AMOUNT = r[1];
       subs.unsubscribe();
     });
   }
@@ -42,7 +66,9 @@ export class InstitucionesComponent implements OnInit {
 
   Create() {
     const subs = this.instS.Create(this.InstitucionForm.value).subscribe(r => {
-      console.log(r);
+      if (r.error) { alert(r.msg); return; }
+      this.InstitucionForm.reset();
+      this.INSTITUCIONES.push(r.output)
       subs.unsubscribe();
     })
   }
