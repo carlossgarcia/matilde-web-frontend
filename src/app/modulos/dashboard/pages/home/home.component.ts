b415def4914ee206475bed7c616b59fbee455f28
Dwigth Astacio Hernández <dwigth.astacio.hernandez@gmail.com>
2021-04-01 11:20:01 -0600

    Cambios

--- src/app/modulos/dashboard/pages/home/home.component.ts
+++ src/app/modulos/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EstadisticasService } from '../../services/estadisticas.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public estadisticasS: EstadisticasService) { }
 
   ngOnInit(): void {
+    this.estadisticasS.home().subscribe(r => { console.log(r) });
   }
 
 }
---
+++ src/app/modulos/dashboard/services/estadisticas.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, retry } from 'rxjs/operators';
+import { HTTP_GENERAL_HEADERS } from 'src/app/classes/headers';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadisticasService {
+
+  headers = new HTTP_GENERAL_HEADERS();
+  constructor(private http: HttpClient, public router: Router) { }
+
+
+  home() {
+    return this.http.get(environment.url + '/estadisticas/home', this.headers.httpOptions).pipe(
+      map((res: any) => {
+        return res;
+      }),
+      retry(1)
+    );
+  }
+}
